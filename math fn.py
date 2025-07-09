8f3ed107d78a1d1df9ef4fab73c518f6cdbee09e
flakysteam <kartikay.narayan.joshi@gmail.com>
2025-07-09 14:08:06 +0530

    Add files via upload

---
+++ math fn.py
@@ -0,0 +1,8 @@
+#Assignment 1 task 1
+a = int(input('Enter your first number: '))
+b = int(input('Enter your second number: '))
+
+print('\nAddition: ' , a+b)
+print('Subtraction: ' , a-b)
+print('Multiplication: ' , a*b)
+print('Division: ' , a/b)
